de15eafe92e7c99d97c52879ce784d156fb6aa20
Preethi <selvampreethi360@gmail.com>
2023-05-18 08:24:21 +0530

    decrypt hash from url and show message - toggle hide in forms

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const { hash } = window.location;
+const decryptedMessage = atob(hash.replace("#", ""));
+if (hash !== "") {
+  document.querySelector("#message-show").classList.toggle("hide");
+  document.querySelector("#message-form").classList.toggle("hide");
+  document.querySelector("h1").innerText = decryptedMessage;
+}
+
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
+
+  document.querySelector("#message-form").classList.toggle("hide");
+  document.querySelector("#link-form").classList.toggle("hide");
+
   const input = document.querySelector("input");
   const encrypted = btoa(input.value);
 
   const linkInput = document.querySelector("#link-input");
+  window.location.hash = "";
   linkInput.value = `${window.location}#${encrypted}`;
   linkInput.select();
 });
